8ad971bafdacc29415d97792ee60e37dd824d87b
MisterPika <51848627+MisterPika@users.noreply.github.com>
2019-06-15 15:20:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 	$botToken = ""; // In mezzo a "" scrivi il Token che ti ha dato @botfather
 	$website = "https://api.telegram.org/bot".$botToken; // Non toccare
+	
+	
 	$update = file_get_contents('php://input'); // Non toccare
 	$update = json_decode($update, TRUE); // Non toccare
 
@@ -12,56 +14,62 @@
 	$id = $update['message']['from']['id']; // Variabile id, che permetterà al bot di conoscere il nostro id numerico di telegram.
 	$lingua = $update['message']['from']['language_code']; // Variabile lingua, che permetterà al bot di conoscere la nostra lingua di telegram.
 	$text = $update['message']['text']; // Non toccare
+
 	
+    
+    switch($text)
+    {
+    	case "/help":
+        	sendMessage($chatId,"<b>Comandi del bot:</b>
+            
+/start - Avvia il Bot
+/help - Visualizza questo messaggio
+/dona - Dona per il Bot.");
+            break;
+    	case "/dona":
+        	sendMessage($chatId,"<b>Dona per il Bot</b>
+            
+Hey! Vuoi supportare il Bot? Innanzitutto grazie per il pensiero,
+se vuoi supportare il Bot, manda i soldi alla nostra email PayPal ufficiale:
+            
+dhaab9653@gmail.com
+            
+Non c'è una somma minima da pagare, si accetta quasiasi somma!");
+            break;
+    	default:
+			sendMessage($chatId, "Benvenuto <b>$nome</b> nel <b>MyInfosBOT</b>!
 	
-	switch($text)
+			Grazie a questo Bot, puoi ottenere tutte le info del tuo account!
+	
+			<b>Nome</b>: $nome
+			<b>Cognome</b>: $cognome
+			<b>Username</b>: @$username
+			<b>ID Numerico</b>: $id
+			<b>Lingua</b>: $lingua
+
+ 								<b>Codice sorgente</b>: https://github.com/misterpika/MyInfosBOT");
+            break;  
+    }
+	
+	function sendMessage($chatId,$text) // Inizio funzione sendMessage che permetterà al Bot di inviare un messaggio.
 	{
-		case "💡 Codice sorgente 💡":
-			sendMessage($chatId,"Per ottenere il codice sorgente, visita il profilo GitHub del Developer.
-			
-			https://github.com/misterpika/MyInfosBOT/");
-			break;
-		default:
-	$tastieraavvio = '["💡 Codice sorgente 💡"]'; // Tastiera che apparirà non appena si cliccherà /start.
-	sendMessage($chatId, "Benvenuto <b>$nome</b> nel <b>MyInfosBOT</b>!
-	
-	Grazie a questo Bot, puoi ottenere tutte le info del tuo account!
-	
-	<b>Nome</b>: $nome
-	<b>Cognome</b>: $cognome
-	<b>Username</b>: @$username
-	<b>ID Numerico</b>: $id
-	<b>Lingua</b>: $lingua",$tastieraavvio);
-			break;
-	}
-	
-	
-	
-	
-	
-	
-	
-	$tastieraavvio = '["💡 Codice sorgente 💡"]'; // Tastiera che apparirà non appena si cliccherà /start.
-	sendMessage($chatId, "Benvenuto <b>$nome</b> nel <b>MyInfosBOT</b>!
-	
-	Grazie a questo Bot, puoi ottenere tutte le info del tuo account!
-	
-	<b>Nome</b>: $nome
-	<b>Cognome</b>: $cognome
-	<b>Username</b>: @$username
-	<b>ID Numerico</b>: $id
-	<b>Lingua</b>: $lingua",$tastieraavvio); // Il messaggio che il bot risponderà quando si invierà un comando al Bot.
-	
-	
-	
-	function sendMessage($chatId,$text,$tastiera) // Inizio funzione sendMessage che permetterà al Bot di inviare un messaggio.
-	{
-		if(isset($tastiera))
-		{
-			$tastierino = '&reply_markup={"keyboard":['.$tastiera.'],"resize_keyboard":true}'; 
-		}
 		$url = $GLOBALS[website]."/sendMessage?chat_id=$chatId&parse_mode=HTML&text=".urlencode($text);
 		file_get_contents($url);
 	} // Fine funzione sendMessage.
 	
+    function MenuPrincipale($chatId,$text)
+    {
+	sendMessage($chatId, "Benvenuto <b>$nome</b> nel <b>MyInfosBOT</b>!
+	
+	Grazie a questo Bot, puoi ottenere tutte le info del tuo account!
+	
+	<b>Nome</b>: $nome
+	<b>Cognome</b>: $cognome
+	<b>Username</b>: @$username
+	<b>ID Numerico</b>: $id
+	<b>Lingua</b>: $lingua
+
+ <b>Codice sorgente</b>: https://github.com/misterpika/MyInfosBOT");
+    }
+    
 ?>
